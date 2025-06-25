1a89a2bceea71927fe8a2d7187c1f49d44fce575
Ramesh <ramesh.pandey@solita.fi>
2025-06-26 00:06:32 +0300

    Initial Commit

---
+++ EUCountriesAppTests/EUCountriesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  EUCountriesAppTests.swift
+//  EUCountriesAppTests
+//
+//  Created by Ramesh Pandey on 26.6.2025.
+//
+
+import Testing
+
+struct EUCountriesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
